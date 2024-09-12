6ee99a6b291df022a12a63e6683b9993411529a1
harsh6754 <agarwalharsh6583@gmail.com>
2024-09-12 10:21:42 +0530

    Vendors Page

--- src/components/Shopkeeper/ShopList.js
+++ src/components/Shopkeeper/ShopList.js
@@ -4,6 +4,7 @@ import { IoAdd } from "react-icons/io5";
 import ShopListImage from '../../assets/ShopListImage1.png';
 import { getShopbyCity } from '../../Services/Operation/vendorAPI';
 import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 
 
 const shopData = [
@@ -72,13 +73,15 @@ export const ShopList = () => {
   getshopData();
 
   console.log("ARRAY..................",shopkeepers);
+
+  const {t} = useTranslation();
   
   return (
     <div className='w-10/12 flex flex-col justify-center space-y-8 p-4 mx-auto'>
 
         <div className='flex flex-col text-[#174B3A] '>
-            <p className='text-4xl mt-14 leading-10 font-semibold text-[#174B3A]'>Here are nearby Shopkeepers !</p>
-            <p className='mt-2 text-[15px] text-[#174B3A] '>We have customized the list of shopkeepers based on your location!</p>
+            <p className='text-4xl mt-14 leading-10 font-semibold text-[#174B3A]'>{t("Here are nearby Shopkeepers")} !</p>
+            <p className='mt-2 text-[15px] text-[#174B3A] '>{t("We have customized the list of shopkeepers based on your location")}!</p>
         </div>
 
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
@@ -93,7 +96,7 @@ export const ShopList = () => {
           onClick={() => setShowAll(!showAll)}
           className='bg-[#F19A3E] w-[100px] text-[16px] text-white py-2 px-4 rounded'
         >
-          {showAll ? 'See Less' : 'See All'}
+          {showAll ? t('See Less') : t('See All')}
         </button>
 
         <hr className='border-[1px] border-black/50'/>
@@ -102,12 +105,12 @@ export const ShopList = () => {
             {/* Card */}
             <div className='bg-[#2EC4B6EB]  shadow-[0_3px_0_0] shadow-[#2EC4B6EB]/50  max-w-[90%] w-[700px] flex flex-col px-10 py-8 rounded-md'>
 
-              <p className='text-white text-[24px]'>Start selling <span className='text-[#174B3A]'>E-waste</span> Now!</p>
-              <p className='text-[16px] mt-1'>Add your product, fill basics details and you are all set to sell your product!</p>
+              <p className='text-white text-[24px]'>{t("Start selling")} <span className='text-[#174B3A]'>{t("E-waste")}</span> {t("Now")}!</p>
+              <p className='text-[16px] mt-1'>{t("Add your product, fill basics details and you are all set to sell your product")}!</p>
 
               <div className='bg-[#F19A3E] text-white rounded-md gap-2 flex justify-center items-center w-[200px] px-[8px] py-[8px] mt-4'>
               <IoAdd  className=" text-[20px]" />
-              <p className='text-[16px]' >Add Product Now</p>
+              <p className='text-[16px]' >{t("Add Product Now")}</p>
               </div>
 
              </div> 
